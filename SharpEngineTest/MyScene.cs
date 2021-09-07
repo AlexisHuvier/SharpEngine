7b0676c7654d747942b1165d9782450b0557e999
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-07 15:32:13 +0200

    Update Test

---
+++ SharpEngineTest/MyScene.cs
@@ -0,0 +1,24 @@
+﻿using SharpEngine;
+
+namespace SharpEngineTest
+{
+    class MyScene: Scene
+    {
+        public MyScene(): base()
+        {}
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            System.Console.WriteLine("INITIALISE MY SCENE");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            System.Console.WriteLine($"{gameTime.elapsedGameTime} - {1 / gameTime.elapsedGameTime.TotalSeconds} FPS");
+        }
+    }
+}

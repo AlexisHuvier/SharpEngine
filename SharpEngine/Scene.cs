0047c6bb03b7ac5b4d0e3838ba3c2a58a9903967
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-07 15:29:44 +0200

    Create Scene

---
+++ SharpEngine/Scene.cs
@@ -0,0 +1,45 @@
+﻿namespace SharpEngine
+{
+    public class Scene
+    {
+        internal Window window;
+
+        public int id;
+
+        public Scene()
+        {
+            this.window = null;
+            id = -1;
+        }
+
+        public virtual void SetWindow(Window window)
+        {
+            this.window = window;
+        }
+
+        public virtual void Initialize()
+        {
+
+        }
+
+        public virtual void LoadContent()
+        {
+
+        }
+
+        public virtual void UnloadContent()
+        {
+
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(GameTime gameTime)
+        {
+
+        }
+    }
+}

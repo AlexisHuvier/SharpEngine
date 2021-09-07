162711265a65674524304d42782e2ff01bdd73f8
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-07 15:31:50 +0200

    Manage scenes in InternalGame

--- SharpEngine/InternalGame.cs
+++ SharpEngine/InternalGame.cs
@@ -24,6 +24,9 @@ namespace SharpEngine
             graphics.PreferredBackBufferHeight = (int)window.screenSize.y;
             graphics.ApplyChanges();
 
+            foreach (Scene scene in window.scenes)
+                scene.Initialize();
+
             base.Initialize();
         }
 
@@ -31,26 +34,38 @@ namespace SharpEngine
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            foreach (Scene scene in window.scenes)
+                scene.LoadContent();
+
             base.LoadContent();
         }
 
         protected override void UnloadContent()
         {
+            foreach (Scene scene in window.scenes)
+                scene.UnloadContent();
+
             base.UnloadContent();
         }
 
-        protected override void Update(GameTime gameTime)
+        protected override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            if (window.currentScene != -1)
+                window.scenes[window.currentScene].Update(GameTime.FromMonogameGameTime(gameTime));
+
             base.Update(gameTime);
         }
 
-        protected override void Draw(GameTime gameTime)
+        protected override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
         {
             GraphicsDevice.Clear(window.backgroundColor.ToMonoGameColor());
 
             spriteBatch.Begin();
+            if (window.currentScene != -1)
+                window.scenes[window.currentScene].Draw(GameTime.FromMonogameGameTime(gameTime));
             spriteBatch.End();
 
             base.Draw(gameTime);

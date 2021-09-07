13acfb28902b97841c584582e9f50b6a0d5755ce
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-08 00:39:54 +0200

    Create Component

---
+++ SharpEngine/Components/Component.cs
@@ -0,0 +1,23 @@
+﻿namespace SharpEngine.Components
+{
+    public class Component
+    {
+        protected Entity entity;
+
+        public Component()
+        {
+            entity = null;
+        }
+
+        public virtual void SetEntity(Entity entity)
+        {
+            this.entity = entity;
+        }
+
+        public virtual void Initialize() {}
+        public virtual void LoadContent() {}
+        public virtual void UnloadContent() {}
+        public virtual void Update(GameTime gameTime) {}
+        public virtual void Draw(GameTime gameTime) {}
+    }
+}

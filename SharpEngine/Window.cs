bb52b2709d0c773a6648c5fa36a28bb23e8a46d5
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-07 12:45:14 +0200

    Create screenSize, backgroundColor, mouseVisible and exitWithEscape in Window

--- SharpEngine/Window.cs
+++ SharpEngine/Window.cs
@@ -5,25 +5,44 @@ namespace SharpEngine
     public class Window
     {
         private InternalGame internalGame;
-        private Vec2 size;
+        private Vec2 _internalScreenSize;
+        private bool _internalMouseVisible;
 
-        public Window(Vec2 screenSize)
+        public Color backgroundColor;
+        public bool exitWithEscape;
+        public Vec2 screenSize
         {
-            size = screenSize;
+            get => _internalScreenSize;
+            set
+            {
+                _internalScreenSize = value;
+                if (internalGame != null)
+                {
+                    internalGame.graphics.PreferredBackBufferWidth = (int)value.x;
+                    internalGame.graphics.PreferredBackBufferHeight = (int)value.y;
+                    internalGame.graphics.ApplyChanges();
+                }
+            }
+        }
+        public bool mouseVisible
+        {
+            get => _internalMouseVisible;
+            set
+            {
+                _internalMouseVisible = value;
+                if (internalGame != null)
+                    internalGame.IsMouseVisible = mouseVisible;
+            }
+        }
+
+        public Window(Vec2 screenSize, Color backgroundColor, bool mouseVisible, bool exitWithEscape)
+        {
+            this.screenSize = screenSize;
+            this.backgroundColor = backgroundColor;
+            this.mouseVisible = mouseVisible;
+            this.exitWithEscape = exitWithEscape;
 
             internalGame = new InternalGame(this);
-        }
-
-        public Vec2 ScreenSize()
-        {
-            return size;
-        }
-
-        public void SetScreenSize()
-        {
-            internalGame.graphics.PreferredBackBufferWidth = (int)size.x;
-            internalGame.graphics.PreferredBackBufferHeight = (int)size.y;
-            internalGame.graphics.ApplyChanges();
         }
 
         public void Run()

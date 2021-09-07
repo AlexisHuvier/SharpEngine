87ad87ff8421dfa9d2290f5ec14e914f38c834e9
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-07 15:29:29 +0200

    Create GameTime

---
+++ SharpEngine/Utils/GameTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SharpEngine
+{
+    public class GameTime
+    {
+        public TimeSpan totalGameTime { get; set; }
+        public TimeSpan elapsedGameTime { get; set; }
+        public bool isRunningSlowly { get; set; }
+
+        public GameTime(TimeSpan totalGameTime, TimeSpan elapsedGameTime, bool isRunningSlowly)
+        {
+            this.totalGameTime = totalGameTime;
+            this.elapsedGameTime = elapsedGameTime;
+            this.isRunningSlowly = isRunningSlowly;
+        }
+
+        internal static GameTime FromMonogameGameTime(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            return new GameTime(gameTime.TotalGameTime, gameTime.ElapsedGameTime, gameTime.IsRunningSlowly);
+        }
+    }
+}

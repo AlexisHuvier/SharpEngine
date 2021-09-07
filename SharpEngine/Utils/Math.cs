f9a0d2f2d0b40ac896269dba258bf7d236e6fee6
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-07 12:41:06 +0200

    Create Math

---
+++ SharpEngine/Utils/Math.cs
@@ -0,0 +1,24 @@
+﻿namespace SharpEngine
+{
+    class Math
+    {
+        public static int Clamp(int value, int mini = int.MinValue, int maxi = int.MaxValue)
+        {
+            if (value < mini)
+                return mini;
+            else if (value > maxi)
+                return maxi;
+            else
+                return value;
+        }
+        public static float Clamp(float value, float mini = float.MinValue, float maxi = float.MaxValue)
+        {
+            if (value < mini)
+                return mini;
+            else if (value > maxi)
+                return maxi;
+            else
+                return value;
+        }
+    }
+}

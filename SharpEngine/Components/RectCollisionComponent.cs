5fcb4e16aa4331b4fdbb21671329a7b9d2961afd
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-19 17:40:49 +0200

    Create RectCollisionComponent.cs

---
+++ SharpEngine/Components/RectCollisionComponent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SharpEngine.Components
+{
+    public class RectCollisionComponent: Component
+    {
+        public Vec2 size;
+        public Vec2 offset;
+
+        public RectCollisionComponent(params object[] parameters): base(parameters)
+        {
+            size = new Vec2(1);
+            offset = new Vec2(0);
+
+            if (parameters.Length >= 1 && parameters[0] is Vec2 si)
+                size = si;
+            if (parameters.Length >= 2 && parameters[1] is Vec2 off)
+                offset = off;
+        }
+
+        public bool can_go(Vec2 position, string cause)
+        {
+            var rect = new Rectangle((int) (position.x + offset.x - size.x / 2), (int) (position.y + offset.y - size.y / 2), (int)size.x, (int)size.y);
+            foreach(Entity e in entity.scene.GetEntities())
+            {
+                if (e != entity)
+                {
+                    if (e.GetComponent<RectCollisionComponent>() is RectCollisionComponent rcc && e.GetComponent<TransformComponent>() is TransformComponent tc)
+                    {
+                        var erect = new Rectangle((int)(tc.position.x + rcc.offset.x - rcc.size.x / 2), (int)(tc.position.y + rcc.offset.y - rcc.size.y / 2), (int)rcc.size.x, (int)rcc.size.y);
+                        if (erect.Intersects(rect))
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"RectCollisionComponent(size={size}, offset={offset})";
+        }
+    }
+}

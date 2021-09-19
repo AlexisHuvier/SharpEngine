e4cea38f296934ce7558ab2e8a69ecefece64bd6
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-19 17:41:00 +0200

    Update Test

--- SharpEngineTest/MyScene.cs
+++ SharpEngineTest/MyScene.cs
@@ -9,11 +9,19 @@ namespace SharpEngineTest
         {
 
             var ent = new Entity();
-            ent.AddComponent<TransformComponent>(new Vec2(100), new Vec2(2, 2), 45);
+            ent.AddComponent<TransformComponent>(new Vec2(100), new Vec2(2, 2));
             ent.AddComponent<SpriteComponent>("test");
+            ent.AddComponent<ControlComponent>();
+            ent.AddComponent<RectCollisionComponent>(new Vec2(90));
             AddEntity(ent);
 
-            foreach(Component comp in entities[0].GetComponents())
+            var ent2 = new Entity();
+            ent2.AddComponent<TransformComponent>(new Vec2(300), new Vec2(1, 1));
+            ent2.AddComponent<SpriteComponent>("test");
+            ent2.AddComponent<RectCollisionComponent>(new Vec2(45));
+            AddEntity(ent2);
+
+            foreach (Component comp in entities[0].GetComponents())
                 System.Console.WriteLine(comp);
         }
     }

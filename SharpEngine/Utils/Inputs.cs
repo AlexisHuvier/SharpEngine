d31a51db63711a8fbf24902874f48ffb8d547dd1
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-19 17:39:51 +0200

    Create MouseButtons Inputs

---
+++ SharpEngine/Utils/Inputs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine.Inputs
+{
+    public enum MouseButton
+    {
+        LEFT,
+        MIDDLE,
+        RIGHT
+    }
+}

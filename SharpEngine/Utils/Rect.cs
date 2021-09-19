be9759b9d0301b7ccd8f4167492489f6a35bc4b2
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-19 17:39:38 +0200

    Create Rect.cs

---
+++ SharpEngine/Utils/Rect.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SharpEngine
+{
+    public class Rect
+    {
+        public Vec2 position;
+        public Vec2 size;
+
+        public Rect(Vec2 pos, Vec2 siz)
+        {
+            position = pos;
+            size = siz;
+        }
+
+        public Rect(float x, float y, Vec2 size): this(new Vec2(x, y), size) { }
+        public Rect(Vec2 pos, float width, float height) : this(pos, new Vec2(width, height)) { }
+        public Rect(float x, float y, float width, float height) : this(new Vec2(x, y), new Vec2(width, height)) { }
+
+        internal Rectangle ToMonoGameRectangle()
+        {
+            return new Rectangle((int) position.x, (int) position.y, (int) size.x, (int) size.y);
+        }
+
+        public static implicit operator Rect(Rectangle rectangle)
+        {
+            return new Rect(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+        }
+    }
+}

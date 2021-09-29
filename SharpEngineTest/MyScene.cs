d93490d79ee8a47eafd2616ebff7ab00df0cf4db
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-29 15:46:52 +0200

    Update test

--- SharpEngineTest/MyScene.cs
+++ SharpEngineTest/MyScene.cs
@@ -9,16 +9,17 @@ namespace SharpEngineTest
         {
 
             var ent = new Entity();
-            ent.AddComponent<TransformComponent>(new Vec2(100), new Vec2(2, 2));
+            ent.AddComponent<TransformComponent>(new Vec2(300, 100), new Vec2(2, 2));
             ent.AddComponent<SpriteComponent>("test");
-            var cc = ent.AddComponent<ControlComponent>(ControlType.FOURDIRECTION);
-            cc.SetKey(ControlKey.DOWN, SharpEngine.Inputs.Key.Z);
+            ent.AddComponent<ControlComponent>(ControlType.CLASSICJUMP);
             ent.AddComponent<RectCollisionComponent>(new Vec2(90));
+            ent.AddComponent<PhysicsComponent>();
             AddEntity(ent);
 
             var ent2 = new Entity();
             ent2.AddComponent<TransformComponent>(new Vec2(300), new Vec2(1, 1));
             ent2.AddComponent<TextComponent>("Testing", "arial", Color.WHITE);
+            ent2.AddComponent<RectCollisionComponent>(new Vec2(20));
             AddEntity(ent2);
 
             foreach (Component comp in entities[0].GetComponents())

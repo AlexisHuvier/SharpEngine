ac9d58ba13301fbe24872f7329201540493507a2
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-30 23:56:00 +0200

    Create Selector.cs

---
+++ SharpEngine/Widgets/Selector.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+
+namespace SharpEngine.Widgets
+{
+    public class Selector: Widget
+    {
+        private readonly List<string> texts;
+        private Button LeftButton;
+        private Button RightButton;
+        private Label text;
+        private string font;
+        private int selected;
+
+        public Selector(params object[] parameters) : base(parameters)
+        {
+            font = "";
+            texts = new List<string>();
+            selected = 0;
+
+            if (parameters.Length >= 2 && parameters[1] is string fnt)
+                font = fnt;
+            if (parameters.Length >= 3)
+            {
+                foreach (string obj in parameters[2..])
+                    texts.Add(obj);
+            }
+        }
+
+        public string GetValue()
+        {
+            return texts[selected];
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+
+            float maxsize = 0;
+            foreach (string text in texts)
+            {
+                float temp = scene.window.fontManager.GetFont(font).MeasureString(text).X;
+                if (maxsize < temp)
+                    maxsize = temp;
+            }
+            LeftButton = new Button(position + new Vec2(20) / 2 - new Vec2(maxsize / 2 + 25, 20 / 2), "<", font, new Vec2(20))
+            {
+                command = (Button b) =>
+                {
+                    if (selected == 0)
+                        selected = texts.Count - 1;
+                    else
+                        selected--;
+                    text.text = texts[selected];
+                }
+            };
+            RightButton = new Button(position + new Vec2(20) / 2 + new Vec2(maxsize / 2 + 10, -20 / 2), ">", font, new Vec2(20))
+            {
+                command = (Button b) =>
+                {
+                    if (selected == texts.Count - 1)
+                        selected = 0;
+                    else
+                        selected++;
+                    text.text = texts[selected];
+                }
+            };
+            text = new Label(position, texts[selected], font);
+
+            LeftButton.scene = scene;
+            RightButton.scene = scene;
+            text.scene = scene;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            LeftButton.Update(gameTime);
+            RightButton.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            if (scene == null)
+                return;
+
+            LeftButton.Draw(gameTime);
+            RightButton.Draw(gameTime);
+            text.Draw(gameTime);
+
+            base.Draw(gameTime);
+        }
+    }
+}

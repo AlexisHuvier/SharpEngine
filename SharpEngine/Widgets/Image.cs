16a6079371a4ecbfda62711078723b73751758a8
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-09-30 18:41:45 +0200

    Create Image.cs

---
+++ SharpEngine/Widgets/Image.cs
@@ -0,0 +1,32 @@
+﻿namespace SharpEngine.Widgets
+{
+    public class Image: Widget
+    {
+        public string texture;
+        public Vec2 size;
+
+        public Image(params object[] parameters): base(parameters)
+        {
+            texture = "";
+            size = null;
+
+            if (parameters.Length >= 2 && parameters[1] is string text)
+                texture = text;
+            if (parameters.Length >= 3 && parameters[2] is Vec2 siz)
+                size = siz;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            if(displayed && texture.Length > 0) {
+                var sprite = scene.window.textureManager.GetTexture(texture);
+                if (size == null)
+                    scene.window.internalGame.spriteBatch.Draw(sprite, new Rect(position - new Vec2(sprite.Width, sprite.Height) / 2, new Vec2(sprite.Width, sprite.Height)).ToMG(), Color.WHITE.ToMG());
+                else
+                    scene.window.internalGame.spriteBatch.Draw(sprite, new Rect(position - size / 2, size).ToMG(), Color.WHITE.ToMG());
+            }
+        }
+    }
+}

68db0fa958eae02488ab6a24cd85d49efc94f84c
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-10-01 14:57:06 +0200

    Add solid property and collisionCallback in RectCollisionComponent

--- SharpEngine/Components/RectCollisionComponent.cs
+++ SharpEngine/Components/RectCollisionComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace SharpEngine.Components
 {
@@ -6,16 +7,22 @@ namespace SharpEngine.Components
     {
         public Vec2 size;
         public Vec2 offset;
+        public bool solid;
+        public Action<Entity, Entity, string> collisionCallback;
 
         public RectCollisionComponent(params object[] parameters): base(parameters)
         {
             size = new Vec2(1);
             offset = new Vec2(0);
+            solid = true;
+            collisionCallback = null;
 
             if (parameters.Length >= 1 && parameters[0] is Vec2 si)
                 size = si;
             if (parameters.Length >= 2 && parameters[1] is Vec2 off)
                 offset = off;
+            if (parameters.Length >= 3 && parameters[2] is bool sol)
+                solid = sol;
         }
 
         public bool CanGo(Vec2 position, string cause)
@@ -29,7 +36,14 @@ namespace SharpEngine.Components
                     {
                         var erect = new Rectangle((int)(tc.position.x + rcc.offset.x - rcc.size.x / 2), (int)(tc.position.y + rcc.offset.y - rcc.size.y / 2), (int)rcc.size.x, (int)rcc.size.y);
                         if (erect.Intersects(rect))
-                            return false;
+                        {
+                            if(solid && rcc.solid)
+                                return false;
+                            if (collisionCallback != null)
+                                collisionCallback(entity, e, cause);
+                            if (rcc.collisionCallback != null)
+                                collisionCallback(e, entity, cause);
+                        }
                     }
                 }
             }

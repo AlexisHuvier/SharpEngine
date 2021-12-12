27f4b32929de80e8a82ba8c7b04e722fdac808fb
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-12-12 13:31:12 +0100

    Create offset in SpriteComponent and TextComponent

--- SharpEngine/Components/SpriteComponent.cs
+++ SharpEngine/Components/SpriteComponent.cs
@@ -9,22 +9,27 @@ namespace SharpEngine.Components
     {
         public string sprite;
         public bool displayed;
+        public Vec2 offset;
 
         /// <summary>
         /// Initialise le Composant.<para/>
         /// -> Paramètre 1 : Nom de la texture (string) ("")<para/>
         /// -> Paramètre 2 : Est affiché (bool) (true)<para/>
+        /// -> Paramètre 3 : Offset (<seealso cref="Vec2"/>) (Vec2(0))<para/>
         /// </summary>
         /// <param name="parameters">Paramètres du Composant</param>
         public SpriteComponent(params object[] parameters): base(parameters)
         {
             sprite = "";
             displayed = true;
+            offset = new Vec2(0);
 
             if (parameters.Length >= 1 && parameters[0] is string spr)
                 sprite = spr;
             if (parameters.Length >= 2 && parameters[1] is bool disp)
                 displayed = disp;
+            if (parameters.Length >= 3 && parameters[2] is Vec2 off)
+                offset = off;
         }
 
         public override void Draw(GameTime gameTime)
@@ -33,13 +38,13 @@ namespace SharpEngine.Components
 
             if (entity.GetComponent<TransformComponent>() is TransformComponent tc && displayed && sprite.Length > 0) {
                 var texture = GetWindow().textureManager.GetTexture(sprite);
-                GetSpriteBatch().Draw(texture, (tc.position - CameraManager.position).ToMG(), null, Color.WHITE.ToMG(), MathHelper.ToRadians(tc.rotation), new Vector2(texture.Width, texture.Height) / 2, tc.scale.ToMG(), Microsoft.Xna.Framework.Graphics.SpriteEffects.None, 1);
+                GetSpriteBatch().Draw(texture, (tc.position + offset - CameraManager.position).ToMG(), null, Color.WHITE.ToMG(), MathHelper.ToRadians(tc.rotation), new Vector2(texture.Width, texture.Height) / 2, tc.scale.ToMG(), Microsoft.Xna.Framework.Graphics.SpriteEffects.None, 1);
             }
         }
 
         public override string ToString()
         {
-            return $"SpriteComponent(sprite={sprite}, displayed={displayed})";
+            return $"SpriteComponent(sprite={sprite}, displayed={displayed}, offset={offset})";
         }
     }
 }
--- SharpEngine/Components/TextComponent.cs
+++ SharpEngine/Components/TextComponent.cs
@@ -9,13 +9,15 @@
         public string font;
         public Color color;
         public bool displayed;
+        public Vec2 offset;
 
         /// <summary>
         /// Initialise le Composant.<para/>
         /// -> Paramètre 1 : Texte (string) ("")<para/>
         /// -> Paramètre 2 : Nom de la police (string) ("")<para/>
         /// -> Paramètre 3 : Couleur du texte (<seealso cref="Color"/>) (Color.BLACK)<para/>
-        /// -> Paramètre 3 : Est affiché (bool) (true)
+        /// -> Paramètre 4 : Est affiché (bool) (true)<para/>
+        /// -> Paramètre 5 : Offset (<seealso cref="Vec2"/>) (Vec2(0))
         /// </summary>
         /// <param name="parameters">Paramètres du Composant</param>
         public TextComponent(params object[] parameters) : base(parameters)
@@ -24,6 +26,7 @@
             font = "";
             color = Color.BLACK;
             displayed = true;
+            offset = new Vec2(0);
 
             if (parameters.Length >= 1 && parameters[0] is string txt)
                 text = txt;
@@ -33,6 +36,8 @@
                 color = clr;
             if (parameters.Length >= 4 && parameters[3] is bool disp)
                 displayed = disp;
+            if (parameters.Length >= 5 && parameters[4] is Vec2 off)
+                offset = off;
         }
 
         public override void Draw(GameTime gameTime)
@@ -42,13 +47,13 @@
             if (entity.GetComponent<TransformComponent>() is TransformComponent tc && displayed && text.Length > 0 && font.Length > 0)
             {
                 var spriteFont = GetWindow().fontManager.GetFont(font);
-                GetSpriteBatch().DrawString(spriteFont, text, (tc.position - CameraManager.position).ToMG(), color.ToMG(), tc.rotation, spriteFont.MeasureString(text) / 2, tc.scale.ToMG(), Microsoft.Xna.Framework.Graphics.SpriteEffects.None, 1);
+                GetSpriteBatch().DrawString(spriteFont, text, (tc.position + offset - CameraManager.position).ToMG(), color.ToMG(), tc.rotation, spriteFont.MeasureString(text) / 2, tc.scale.ToMG(), Microsoft.Xna.Framework.Graphics.SpriteEffects.None, 1);
             }
         }
 
         public override string ToString()
         {
-            return $"TextComponent(text={text}, font={font}, color={color}, displayed={displayed})";
+            return $"TextComponent(text={text}, font={font}, color={color}, displayed={displayed}, offset={offset})";
         }
     }
 }

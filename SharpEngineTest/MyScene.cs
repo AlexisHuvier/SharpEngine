bf035a1345901521fc6a11af5df93a26cf13bbfc
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-12-12 13:31:37 +0100

    Update Test

--- SharpEngineTest/MyScene.cs
+++ SharpEngineTest/MyScene.cs
@@ -9,26 +9,13 @@ namespace SharpEngineTest
     {
         public MyScene(): base()
         {
-            Entity ent2 = new Entity();
-            ent2.AddComponent<TransformComponent>(new Vec2(450, 300));
-            ent2.AddComponent<TileMapComponent>("tiled/map_multilayer.tmx");
-            AddEntity(ent2);
-
             Entity ent = new Entity();
             ent.AddComponent<TransformComponent>(new Vec2(450, 300));
-            ent.AddComponent<RectCollisionComponent>(new Vec2(10), new Vec2(0), true);
+            ent.AddComponent<RectCollisionComponent>(new Vec2(10), new Vec2(0), true, new Vec2(10));
             ent.AddComponent<SpriteComponent>("test");
             ent.AddComponent<ControlComponent>(ControlType.FOURDIRECTION);
-            ent.AddComponent<AutoMovementComponent>(new Vec2(2), 5);
+            ent.AddComponent<TextComponent>("Salut !", "arial", Color.BLACK, true, new Vec2(-10));
             AddEntity(ent);
-
-            Entity ent3 = new Entity();
-            ent3.AddComponent<TransformComponent>(new Vec2(0, 0));
-            ent3.AddComponent<CircleCollisionComponent>(200, new Vec2(0), true);
-            ent3.AddComponent<SpriteComponent>("test");
-            AddEntity(ent3);
-
-            CameraManager.followEntity = ent;
         }
 
         public override void Update(GameTime gameTime)

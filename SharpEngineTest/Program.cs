71ea1d5f0aaae0af42a5251f8775067b8fb94586
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-16 17:22:30 +0200

    Update test

--- SharpEngineTest/Program.cs
+++ SharpEngineTest/Program.cs
@@ -4,8 +4,6 @@ namespace SharpEngineTest
 {
     class Program
     {
-        static bool DontStart() => false;
-
         static void Main(string[] args)
         {
             Window win = new Window(new Vec2(900, 600), Color.CORNFLOWER_BLUE);
@@ -14,8 +12,6 @@ namespace SharpEngineTest
             win.textureManager.AddTexture("spritesheet", "spritesheet.png");
             win.textureManager.AddTexture("flamme", "flamme.gif");
             win.fontManager.AddFont("arial", "C:\\Windows\\Fonts\\arial.ttf");
-
-            win.startCallback = DontStart;
 
             win.AddScene(new MyScene());
             win.Run();

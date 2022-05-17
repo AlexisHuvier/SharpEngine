4468b6114be9d08c2640a13297c08e1af2da092a
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-17 15:34:25 +0200

    Remove IntersectCircleRect of Math

--- SharpEngine/Utils/Math.cs
+++ SharpEngine/Utils/Math.cs
@@ -52,29 +52,5 @@
             else
                 return value;
         }
-
-        public static bool IntersectCircleRect(float cx, float cy, float radius, float rx, float ry, float rw, float rh)
-        {
-
-            // temporary variables to set edges for testing
-            float testX = cx;
-            float testY = cy;
-
-            // which edge is closest?
-            if (cx < rx) testX = rx;      // test left edge
-            else if (cx > rx + rw) testX = rx + rw;   // right edge
-            if (cy < ry) testY = ry;      // top edge
-            else if (cy > ry + rh) testY = ry + rh;   // bottom edge
-
-            // get distance from closest edges
-            float distX = cx - testX;
-            float distY = cy - testY;
-            double distance = System.Math.Sqrt((distX * distX) + (distY * distY));
-
-            // if the distance is less than the radius, collision!
-            if (distance <= radius)
-                return true;
-            return false;
-        }
     }
 }

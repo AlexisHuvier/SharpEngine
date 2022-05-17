030170ef73fef7aa8efac23b576960b70f8c934d
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-17 17:29:18 +0200

    Create SE_BasicWindow in Demos

---
+++ Demos/SE_BasicWindow/MyScene.cs
@@ -0,0 +1,23 @@
+﻿using SharpEngine;
+
+namespace SE_BasicWindow
+{
+    class MyScene : Scene
+    {
+        public MyScene() : base()
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (InputManager.IsKeyPressed(SharpEngine.Inputs.Key.D1))
+                GetWindow().fullscreen = FullScreenType.NO_FULLSCREEN;
+            if (InputManager.IsKeyPressed(SharpEngine.Inputs.Key.D2))
+                GetWindow().fullscreen = FullScreenType.HARDWARE_FULLSCREEN;
+            if (InputManager.IsKeyPressed(SharpEngine.Inputs.Key.D3))
+                GetWindow().fullscreen = FullScreenType.BORDERLESS_FULLSCREEN;
+        }
+    }
+}

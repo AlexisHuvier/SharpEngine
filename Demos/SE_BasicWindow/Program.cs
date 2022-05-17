c4c1bada605e872bd6ff0a07b2db57fa5fd44bfd
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-17 17:29:18 +0200

    Create SE_BasicWindow in Demos

---
+++ Demos/SE_BasicWindow/Program.cs
@@ -0,0 +1,15 @@
+﻿using SharpEngine;
+
+namespace SE_BasicWindow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window win = new Window(new Vec2(900, 600), Color.CORNFLOWER_BLUE, fullscreen: FullScreenType.BORDERLESS_FULLSCREEN);
+
+            win.AddScene(new MyScene());
+            win.Run();
+        }
+    }
+}

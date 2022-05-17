b50d528e66c7ab2603111d67e5d53309dc2a3275
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-17 15:34:42 +0200

    Create ColorTests, MathsTests and RectTests

---
+++ SharpEngineTests/Utils/MathsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpEngine;
+
+namespace SharpEngineTests.Utils
+{
+    [TestClass]
+    public class MathsTests
+    {
+        [TestMethod]
+        public void Degrees_Radians()
+        {
+            Assert.AreEqual(180, Math.ToDegrees(Math.PI));
+            Assert.AreEqual(Math.TWOPI, Math.ToRadians(360));
+            Assert.AreEqual(345, Math.ToDegrees(Math.ToRadians(345)));
+        }
+
+        [TestMethod]
+        public void Distance()
+        {
+            Assert.AreEqual(0.8f, System.MathF.Round(Math.Distance(1.5f, 2.3f), 1));
+            Assert.AreEqual(2, Math.Distance(2, 4));
+        }
+
+        [TestMethod]
+        public void Clamp()
+        {
+            Assert.AreEqual(0, Math.Clamp(-5, 0, 5));
+            Assert.AreEqual(2, Math.Clamp(2, 0, 5));
+            Assert.AreEqual(5, Math.Clamp(10, 0, 5));
+        }
+    }
+}
---
+++ SharpEngineTests/Utils/RectTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpEngine;
+
+namespace SharpEngineTests.Utils
+{
+    [TestClass]
+    public class RectTests
+    {
+        [TestMethod]
+        public void Constructors()
+        {
+            Rect r = new Rect(0, 0, 0, 0);
+            Rect r2 = new Rect(new Vec2(0), new Vec2(0));
+            Rect r3 = new Rect(new Vec2(0), 0, 0);
+            Rect r4 = new Rect(0, 0, new Vec2(0));
+            Assert.AreEqual(r, r2);
+            Assert.AreEqual(r, r3);
+            Assert.AreEqual(r, r4);
+            Assert.AreEqual(new Vec2(0), r.position);
+            Assert.AreEqual(new Vec2(0), r.size);
+        }
+
+        [TestMethod]
+        public void ToMG() => Assert.IsInstanceOfType(new Rect(0, 0, 0, 0).ToMG(), typeof(Microsoft.Xna.Framework.Rectangle));
+    }
+}

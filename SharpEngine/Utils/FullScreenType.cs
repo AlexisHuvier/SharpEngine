321b4e82cbfb316b7e91e86a857a15933a9f2c86
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-17 17:28:40 +0200

    Add fullscreen management in Window

---
+++ SharpEngine/Utils/FullScreenType.cs
@@ -0,0 +1,12 @@
+﻿namespace SharpEngine
+{
+    /// <summary>
+    /// Type de fullscreen utilisé dans <seealso cref="Window"/>
+    /// </summary>
+    public enum FullScreenType
+    {
+        NO_FULLSCREEN,
+        HARDWARE_FULLSCREEN,
+        BORDERLESS_FULLSCREEN
+    }
+}

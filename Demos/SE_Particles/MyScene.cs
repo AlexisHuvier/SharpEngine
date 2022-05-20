478fe7f0623b7abb7ae506b4afe643a269a0df7d
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-20 16:06:07 +0200

    Update SE_Particles Demo

--- Demos/SE_Particles/MyScene.cs
+++ Demos/SE_Particles/MyScene.cs
@@ -10,20 +10,17 @@ namespace SE_Particles
         {
             Entity ent = new Entity();
             ent.AddComponent(new TransformComponent(new Vec2(420, 300)));
-            ParticleUtils.ParticleEmitter particleEmitter = new ParticleUtils.ParticleEmitter(new Color[] { Color.BLUE, Color.RED, Color.YELLOW }, 
-                minDirection:-120, maxDirection:-60, active: true);
-            ent.AddComponent(new ParticleComponent()).AddEmitter(particleEmitter);
+            ParticleUtils.ParticleEmitter firstEmitter = new ParticleUtils.ParticleEmitter(new Color[] { Color.BLUE, Color.RED, Color.YELLOW }, 
+                minDirection:-120, maxDirection:-60, active: true, offset: new Vec2(-30, 0));
+            ParticleUtils.ParticleEmitter secondEmitter = new ParticleUtils.ParticleEmitter(new Color[] { Color.DARK_VIOLET }, minDirection: 0, maxDirection: 360,
+                minSize: 20, maxSize: 20, sizeFunction: ParticleUtils.ParticleParametersFunction.DECREASE, active: true, offset: new Vec2(20, 0)); 
+            ParticleUtils.ParticleEmitter thirdEmitter = new ParticleUtils.ParticleEmitter(new Color[] { Color.GREEN }, minDirection: 0, maxDirection: 360,
+                minSize: 20, maxSize: 20, sizeFunction: ParticleUtils.ParticleParametersFunction.INCREASE, active: true, offset: new Vec2(70, 0));
+            ParticleComponent cmp = ent.AddComponent(new ParticleComponent());
+            cmp.AddEmitter(firstEmitter);
+            cmp.AddEmitter(secondEmitter);
+            cmp.AddEmitter(thirdEmitter);
             AddEntity(ent);
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-
-            if (InputManager.IsKeyPressed(SharpEngine.Inputs.Key.S))
-                entities[0].GetComponent<ParticleComponent>().GetEmitters()[0].active = true;
-            if (InputManager.IsKeyPressed(SharpEngine.Inputs.Key.D))
-                entities[0].GetComponent<ParticleComponent>().GetEmitters()[0].active = false;
         }
     }
 }

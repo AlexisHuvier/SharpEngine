e189f95be7f9d5e49b5834906d47be9e91927c47
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-20 12:05:25 +0200

    Create SE_Particles Demo

---
+++ Demos/SE_Particles/MyScene.cs
@@ -0,0 +1,29 @@
+﻿using SharpEngine;
+using SharpEngine.Components;
+using SharpEngine.Utils;
+
+namespace SE_Particles
+{
+    public class MyScene : Scene
+    {
+        public MyScene() : base()
+        {
+            Entity ent = new Entity();
+            ent.AddComponent(new TransformComponent(new Vec2(420, 300)));
+            ParticleUtils.ParticleEmitter particleEmitter = new ParticleUtils.ParticleEmitter(new Color[] { Color.BLUE, Color.RED, Color.YELLOW }, 
+                minDirection:-120, maxDirection:-60, active: true);
+            ent.AddComponent(new ParticleComponent()).AddEmitter(particleEmitter);
+            AddEntity(ent);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (InputManager.IsKeyPressed(SharpEngine.Inputs.Key.S))
+                entities[0].GetComponent<ParticleComponent>().GetEmitters()[0].active = true;
+            if (InputManager.IsKeyPressed(SharpEngine.Inputs.Key.D))
+                entities[0].GetComponent<ParticleComponent>().GetEmitters()[0].active = false;
+        }
+    }
+}
---
+++ Demos/SE_Particles/Program.cs
@@ -0,0 +1,15 @@
+﻿using SharpEngine;
+
+namespace SE_Particles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window win = new Window(new Vec2(900, 600), Color.CORNFLOWER_BLUE);
+
+            win.AddScene(new MyScene());
+            win.Run();
+        }
+    }
+}

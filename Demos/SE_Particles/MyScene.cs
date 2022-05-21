1ba35909bead7e43e69b1938928a18eb0c347227
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-21 19:58:18 +0200

    Update SE_Particles Demo

--- Demos/SE_Particles/MyScene.cs
+++ Demos/SE_Particles/MyScene.cs
@@ -10,20 +10,22 @@ namespace SE_Particles
         {
             Entity ent = new Entity();
             ent.AddComponent(new TransformComponent(new Vec2(420, 300)));
-            ParticleUtils.ParticleEmitter firstEmitter = new ParticleUtils.ParticleEmitter(new Color[] { Color.BLUE, Color.RED, Color.YELLOW }, 
-                minDirection:-120, maxDirection:-60, active: true, offset: new Vec2(-30, 0));
-            ParticleUtils.ParticleEmitter secondEmitter = new ParticleUtils.ParticleEmitter(new Color[] { Color.DARK_VIOLET }, minDirection: 0, maxDirection: 360,
-                minSize: 20, maxSize: 20, sizeFunction: ParticleUtils.ParticleParametersFunction.DECREASE, active: true, offset: new Vec2(20, 0)); 
-            ParticleUtils.ParticleEmitter thirdEmitter = new ParticleUtils.ParticleEmitter(new Color[] { Color.GREEN }, minDirection: 0, maxDirection: 360,
-                minSize: 20, maxSize: 20, sizeFunction: ParticleUtils.ParticleParametersFunction.INCREASE, active: true, offset: new Vec2(70, 0));
-            ParticleUtils.ParticleEmitter fourEmitter = new ParticleUtils.ParticleEmitter(new Color[] { Color.RED }, minDirection: -140, maxDirection: -40,
-                endColors: new Color[] { Color.YELLOW }, minSize: 10, maxSize: 10, sizeFunction: ParticleUtils.ParticleParametersFunction.DECREASE, active: true, 
-                offset: new Vec2(-80, 0));
+            ParticleUtils.ParticleEmitter[] emitters = new ParticleUtils.ParticleEmitter[] {
+                new ParticleUtils.ParticleEmitter(new Color[] { Color.BLUE, Color.RED, Color.YELLOW }, minDirection:-120, maxDirection:-60, active: true,
+                    offset: new Vec2(-30, 0)),
+                new ParticleUtils.ParticleEmitter(new Color[] { Color.DARK_VIOLET }, minDirection: 0, maxDirection: 360, minSize: 20, maxSize: 20,
+                    sizeFunction: ParticleUtils.ParticleParametersFunction.DECREASE, active: true, offset: new Vec2(20, 0)),
+                new ParticleUtils.ParticleEmitter(new Color[] { Color.GREEN }, minDirection: 0, maxDirection: 360, minSize: 20, maxSize: 20,
+                    sizeFunction: ParticleUtils.ParticleParametersFunction.INCREASE, active: true, offset: new Vec2(70, 0)),
+                new ParticleUtils.ParticleEmitter(new Color[] { Color.RED }, minDirection: -140, maxDirection: -40, endColors: new Color[] { Color.YELLOW }, minSize: 10,
+                    maxSize: 10, sizeFunction: ParticleUtils.ParticleParametersFunction.DECREASE, active: true, offset: new Vec2(-80, 0)),
+                new ParticleUtils.ParticleEmitter(new Color[] { Color.BLUE }, minDirection: -90, maxDirection: -90, active: true, offset: new Vec2(120, 0), 
+                    spawnSize: new Vec2(40))
+            };
             ParticleComponent cmp = ent.AddComponent(new ParticleComponent());
-            cmp.AddEmitter(firstEmitter);
-            cmp.AddEmitter(secondEmitter);
-            cmp.AddEmitter(thirdEmitter);
-            cmp.AddEmitter(fourEmitter);
+            foreach (ParticleUtils.ParticleEmitter emitter in emitters)
+                cmp.AddEmitter(emitter);
+            ent.AddComponent(new ControlComponent());
             AddEntity(ent);
         }
     }

e8b585c55a20e0b642c02aec8bc9ab76a39f0fbb
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-07-23 03:16:08 +0200

    Create SE_Tiled Demo

---
+++ Demos/SE_Tiled/MyScene.cs
@@ -0,0 +1,16 @@
+﻿using SharpEngine;
+using SharpEngine.Components;
+using SharpEngine.Entities;
+using SharpEngine.Utils;
+
+namespace SE_Tiled;
+public sealed class MyScene : Scene
+{
+    public MyScene()
+    {
+        var ent = new Entity();
+        ent.AddComponent(new TransformComponent(new Vec2(420, 300)));
+        ent.AddComponent(new TileMapComponent("Resources/map.tmx"));
+        AddEntity(ent);
+    }
+}
---
+++ Demos/SE_Tiled/Program.cs
@@ -0,0 +1,16 @@
+﻿using SharpEngine;
+using SharpEngine.Utils;
+
+namespace SE_Tiled;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var win = new Window(new Vec2(900, 600), Color.CornflowerBlue);
+        
+
+        win.AddScene(new MyScene());
+        win.Run();
+    }
+}

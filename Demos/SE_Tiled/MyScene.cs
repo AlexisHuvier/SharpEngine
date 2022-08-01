220888100f771c9e6a2116322456058aaed8bfb5
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-08-01 02:34:22 +0200

    Update SE_Tiled Demo

--- Demos/SE_Tiled/MyScene.cs
+++ Demos/SE_Tiled/MyScene.cs
@@ -1,6 +1,7 @@
 ﻿using SharpEngine;
 using SharpEngine.Components;
 using SharpEngine.Entities;
+using SharpEngine.Managers;
 using SharpEngine.Utils;
 
 namespace SE_Tiled;
@@ -12,5 +13,24 @@ public sealed class MyScene : Scene
         ent.AddComponent(new TransformComponent(new Vec2(420, 300)));
         ent.AddComponent(new TileMapComponent("Resources/map.tmx"));
         AddEntity(ent);
+
+        var ent2 = new Entity();
+        ent2.AddComponent(new TransformComponent(new Vec2(420, 300)));
+        ent2.AddComponent(new ControlComponent(ControlType.FourDirection));
+        ent2.AddComponent(new SpriteComponent("sprite0"));
+        AddEntity(ent2);
+
+        CameraManager.FollowEntity = ent2;
+    }
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+
+        if (!InputManager.IsKeyPressed(Key.A)) return;
+        
+        Console.WriteLine($"SE Version : {DebugManager.GetSharpEngineVersion()}");
+        Console.WriteLine($"Monogame Version : {DebugManager.GetMonogameVersion()}");
+        Console.WriteLine($"FPS : {DebugManager.GetFps()}");
+        Console.WriteLine($"GC Memory : {DebugManager.GetGcMemory()}");
     }
 }
--- Demos/SE_Tiled/Program.cs
+++ Demos/SE_Tiled/Program.cs
@@ -8,8 +8,9 @@ internal static class Program
     private static void Main()
     {
         var win = new Window(new Vec2(900, 600), Color.CornflowerBlue);
-        
 
+        win.TextureManager.AddTexture("sprite0", "Resources/sprite0.png");
+        
         win.AddScene(new MyScene());
         win.Run();
     }

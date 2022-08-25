86638f858c400ca5b043c1e8600707b551ca9c91
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-08-25 18:08:57 +0200

    Update SE_BasicWindow Demo

--- Demos/SE_BasicWindow/Program.cs
+++ Demos/SE_BasicWindow/Program.cs
@@ -1,5 +1,8 @@
-﻿using SharpEngine;
+﻿using ImGuiNET;
+using SharpEngine;
+using SharpEngine.Managers;
 using SharpEngine.Utils;
+using SharpEngine.Utils.Control;
 using SharpEngine.Utils.Math;
 
 namespace SE_BasicWindow;
@@ -8,7 +11,24 @@ internal static class Program
 {
     private static void Main()
     {
-        var win = new Window(new Vec2(900, 600), Color.CornflowerBlue);
+        var win = new Window(new Vec2(900, 600), Color.CornflowerBlue)
+        {
+            RenderImGui = () =>
+            {
+                {
+                    ImGui.Text($"SE Version : {DebugManager.GetSharpEngineVersion()}");
+                    ImGui.Text($"Monogame Version : {DebugManager.GetMonogameVersion()}");
+                    ImGui.Text($"FPS : {DebugManager.GetFps()}");
+                    ImGui.Text($"GC Memory : {DebugManager.GetGcMemory()}");
+                
+                    ImGui.Text($"Connected GamePad : {InputManager.IsGamePadConnected(GamePadIndex.One)}");
+                    ImGui.Text($"1A Pressed : {InputManager.IsGamePadButtonDown(GamePadIndex.One, GamePadButton.A)}");
+                    ImGui.Text($"2A Pressed : {InputManager.IsGamePadButtonDown(GamePadIndex.Two, GamePadButton.A)}");
+                    ImGui.Text($"Left Trigger Value : {InputManager.GetGamePadTrigger(GamePadIndex.One, GamePadTrigger.Left)}");
+                    ImGui.Text($"Left X Axis Value : {InputManager.GetGamePadJoyStickAxis(GamePadIndex.One, GamePadJoyStickAxis.LeftX)}");
+                }
+            }
+        };
 
         win.AddScene(new MyScene());
         win.Run();

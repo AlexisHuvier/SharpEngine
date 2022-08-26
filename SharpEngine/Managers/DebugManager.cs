6bdd0e5e8fd80b8df2d2532ec244f83d9fc8fc98
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-08-26 09:29:58 +0200

    Optimize DebugManager

--- SharpEngine/Managers/DebugManager.cs
+++ SharpEngine/Managers/DebugManager.cs
@@ -12,9 +12,12 @@ public static class DebugManager
     private static int _frameCounter;
     private static TimeSpan _elapsedTime = TimeSpan.Zero;
 
+    private static readonly string MonoGameVersion = System.Diagnostics.FileVersionInfo
+        .GetVersionInfo(typeof(Microsoft.Xna.Framework.Game).Assembly.Location).FileVersion;
+
     public static int GetFps() => _frameRate;
     public static long GetGcMemory() => GC.GetTotalMemory(false);
-    public static string GetMonogameVersion() => System.Diagnostics.FileVersionInfo.GetVersionInfo(typeof(Microsoft.Xna.Framework.Game).Assembly.Location).FileVersion;
+    public static string GetMonogameVersion() => MonoGameVersion;
     public static string GetSharpEngineVersion() => "0.10.0";
 
     internal static void Update(GameTime gameTime)

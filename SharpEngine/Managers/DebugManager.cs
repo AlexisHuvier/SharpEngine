f91ea19e880ca1973ddd4a4248b24147fba8ce4b
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-09-02 17:46:29 +0200

    V 0.10.2

--- SharpEngine/Managers/DebugManager.cs
+++ SharpEngine/Managers/DebugManager.cs
@@ -18,7 +18,7 @@ public static class DebugManager
     public static int GetFps() => _frameRate;
     public static long GetGcMemory() => GC.GetTotalMemory(false);
     public static string GetMonogameVersion() => MonoGameVersion;
-    public static string GetSharpEngineVersion() => "0.10.1";
+    public static string GetSharpEngineVersion() => "0.10.2";
 
     internal static void Update(GameTime gameTime)
     {

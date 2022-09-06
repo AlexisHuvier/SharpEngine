ec3bcf13ef5ccba2d0d03c8e7431203360edc70e
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-09-06 11:03:58 +0200

    Add FlipX and FlipY in SpriteComponent

--- SharpEngine/Components/SpriteComponent.cs
+++ SharpEngine/Components/SpriteComponent.cs
@@ -16,6 +16,8 @@ public class SpriteComponent: Component
     public string Sprite;
     public bool Displayed;
     public Vec2 Offset;
+    public bool FlipX;
+    public bool FlipY;
 
     /// <summary>
     /// Initialise le Composant.
@@ -23,11 +25,15 @@ public class SpriteComponent: Component
     /// <param name="sprite">Nom de la texture</param>
     /// <param name="displayed">Est affiché</param>
     /// <param name="offset">Décalage de la position du Sprite (Vec2(0))</param>
-    public SpriteComponent(string sprite, bool displayed = true, Vec2 offset = null)
+    /// <param name="flipX">Si le sprite est retourné en X</param>
+    /// <param name="flipY">Si le sprite est retourné en Y</param>
+    public SpriteComponent(string sprite, bool displayed = true, Vec2 offset = null, bool flipX = false, bool flipY = false)
     {
         Sprite = sprite;
         Displayed = displayed;
         Offset = offset ?? Vec2.Zero;
+        FlipX = flipX;
+        FlipY = flipY;
     }
 
     public override void Draw(GameTime gameTime)
@@ -35,9 +41,15 @@ public class SpriteComponent: Component
         base.Draw(gameTime);
 
         if (Entity.GetComponent<TransformComponent>() is not { } tc || !Displayed || Sprite.Length <= 0) return;
+
+        var effects = SpriteEffects.None;
+        if (FlipX)
+            effects |= SpriteEffects.FlipHorizontally;
+        if (FlipY)
+            effects |= SpriteEffects.FlipVertically;
         
         var texture = Entity.Scene.Window.TextureManager.GetTexture(Sprite);
-        Renderer.RenderTexture(Entity.Scene.Window, texture, tc.Position + Offset - CameraManager.Position, null, Color.White, MathHelper.ToRadians(tc.Rotation), new Vec2(texture.Width / 2f, texture.Height / 2f), tc.Scale, SpriteEffects.None, 1);
+        Renderer.RenderTexture(Entity.Scene.Window, texture, tc.Position + Offset - CameraManager.Position, null, Color.White, MathHelper.ToRadians(tc.Rotation), new Vec2(texture.Width / 2f, texture.Height / 2f), tc.Scale, effects, 1);
     }
 
     public override string ToString() => $"SpriteComponent(sprite={Sprite}, displayed={Displayed}, offset={Offset})";

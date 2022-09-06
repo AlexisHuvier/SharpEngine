4d1438ae2e02a541796267c261f7a9c71592ee72
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-09-06 18:22:50 +0200

    Add SourceRect, FlipX, FlipY and Scale in Image

--- SharpEngine/Widgets/Image.cs
+++ SharpEngine/Widgets/Image.cs
@@ -1,4 +1,5 @@
-﻿using SharpEngine.Core;
+﻿using Microsoft.Xna.Framework.Graphics;
+using SharpEngine.Core;
 using SharpEngine.Utils;
 using SharpEngine.Utils.Math;
 
@@ -11,6 +12,10 @@ public class Image : Widget
 {
     public string Texture;
     public Vec2 Size;
+    public Rect SourceRect;
+    public bool FlipX;
+    public bool FlipY;
+    public Vec2 Scale;
 
     /// <summary>
     /// Initialise le Widget.
@@ -18,10 +23,19 @@ public class Image : Widget
     /// <param name="position">Position (Vec2(0))</param>
     /// <param name="texture">Nom de la texture</param>
     /// <param name="size">Taille de l'image</param>
-    public Image(Vec2 position = null, string texture = "", Vec2 size = null) : base(position)
+    /// <param name="sourceRect">Partie de la texture à afficher</param>
+    /// <param name="flipX">Si l'image est retourné en X</param>
+    /// <param name="flipY">Si l'image est retourné en Y</param>
+    /// <param name="scale">Scale de l'image</param>
+    public Image(Vec2 position = null, string texture = "", Vec2 size = null, Rect sourceRect = null, 
+        bool flipX = false, bool flipY = false, Vec2 scale = null) : base(position)
     {
         Texture = texture;
         Size = size;
+        SourceRect = sourceRect;
+        FlipX = flipX;
+        FlipY = flipY;
+        Scale = scale ?? Vec2.One;
     }
 
     public override void Draw(GameTime gameTime)
@@ -33,9 +47,17 @@ public class Image : Widget
         var sprite = Scene.Window.TextureManager.GetTexture(Texture);
         var realPosition = Parent != null ? Position + Parent.Position : Position;
 
-        Renderer.RenderTexture(Scene.Window, sprite,
-            Size == null
-                ? new Rect(realPosition - new Vec2(sprite.Width, sprite.Height) / 2, new Vec2(sprite.Width, sprite.Height))
-                : new Rect(realPosition - Size / 2, Size), Color.White);
+        var effects = SpriteEffects.None;
+        if (FlipX)
+            effects |= SpriteEffects.FlipHorizontally;
+        if (FlipY)
+            effects |= SpriteEffects.FlipVertically;
+
+        if (SourceRect == null)
+            Renderer.RenderTexture(Scene.Window, sprite, realPosition, null, Color.White, 0,
+                Size == null ? new Vec2(sprite.Width, sprite.Height) / 2 : Size  / 2, Scale, effects, 1);
+        else
+            Renderer.RenderTexture(Scene.Window, sprite, realPosition, SourceRect, Color.Wheat, 0, SourceRect.Size / 2,
+                Scale, effects, 1);
     }
 }

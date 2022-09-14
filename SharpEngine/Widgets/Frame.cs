8c190cad89239967358ae303ac532fe462b40314
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-09-14 14:08:42 +0200

    Add BackgroundColor in Frame

--- SharpEngine/Widgets/Frame.cs
+++ SharpEngine/Widgets/Frame.cs
@@ -6,22 +6,26 @@ namespace SharpEngine.Widgets;
 
 public class Frame: Widget
 {
-    public Color Color;
+    public Color BorderColor;
     public Vec2 Size;
     public Vec2 BorderSize;
-
+    public Color BackgroundColor;
+    
+    
     /// <summary>
     /// Initialise le Widget.
     /// </summary>
     /// <param name="position">Position (Vec2(0))</param>
     /// <param name="size">Taille du rectangle (Vec2(5))</param>
     /// <param name="borderSize">Taille de la bordure (Vec2(3))</param>
-    /// <param name="color">Couleur du rectangle</param>
-    public Frame(Vec2 position = null, Vec2 size = null, Vec2 borderSize = null, Color color = null) : base(position)
+    /// <param name="borderColor">Couleur de la bordure (Color.Black)</param>
+    /// <param name="backgroundColor">Couleur de fond (Transparent)</param>
+    public Frame(Vec2 position = null, Vec2 size = null, Vec2 borderSize = null, Color borderColor = null, Color backgroundColor = null) : base(position)
     {
-        Color = color ?? Color.Black;
+        BorderColor = borderColor ?? Color.Black;
         Size = size ?? new Vec2(5);
         BorderSize = borderSize ?? new Vec2(3);
+        BackgroundColor = backgroundColor;
     }
 
     public override void Draw(GameTime gameTime)
@@ -32,14 +36,26 @@ public class Frame: Widget
 
         var texture = Scene.Window.TextureManager.GetTexture("blank");
         var realPosition = Parent != null ? Position + Parent.GetRealPosition() : Position;
-        
-        Renderer.RenderTexture(Scene.Window, texture, new Rect(realPosition - Size / 2, Size.X, BorderSize.Y), Color);
-        Renderer.RenderTexture(Scene.Window, texture, new Rect(realPosition - Size / 2, BorderSize.X, Size.Y), Color);
-        Renderer.RenderTexture(Scene.Window, texture,
-            new Rect(realPosition.X - Size.X / 2, realPosition.Y + (Size.Y - BorderSize.Y) - Size.Y / 2, Size.X,
-                BorderSize.Y), Color);
-        Renderer.RenderTexture(Scene.Window, texture,
-            new Rect(realPosition.X + (Size.X - BorderSize.X) - Size.X / 2, realPosition.Y - Size.Y / 2, BorderSize.X,
-                Size.Y), Color);
+
+        if (BackgroundColor == null)
+        {
+            Renderer.RenderTexture(Scene.Window, texture, new Rect(realPosition - Size / 2, Size.X, BorderSize.Y),
+                BorderColor);
+            Renderer.RenderTexture(Scene.Window, texture, new Rect(realPosition - Size / 2, BorderSize.X, Size.Y),
+                BorderColor);
+            Renderer.RenderTexture(Scene.Window, texture,
+                new Rect(realPosition.X - Size.X / 2, realPosition.Y + (Size.Y - BorderSize.Y) - Size.Y / 2, Size.X,
+                    BorderSize.Y), BorderColor);
+            Renderer.RenderTexture(Scene.Window, texture,
+                new Rect(realPosition.X + (Size.X - BorderSize.X) - Size.X / 2, realPosition.Y - Size.Y / 2,
+                    BorderSize.X,
+                    Size.Y), BorderColor);
+        }
+        else
+        {
+            Renderer.RenderTexture(Scene.Window, texture, new Rect(realPosition - Size / 2, Size), BorderColor);
+            var internalSize = Size - BorderSize * 2;
+            Renderer.RenderTexture(Scene.Window, texture, new Rect(realPosition - internalSize / 2, internalSize), BackgroundColor);
+        }
     }
 }

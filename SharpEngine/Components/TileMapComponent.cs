862bf8f755aa661241670339aeffa550cfdc657b
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-05-30 23:40:51 +0200

    Big optimization on TileMapComponent

--- SharpEngine/Components/TileMapComponent.cs
+++ SharpEngine/Components/TileMapComponent.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework.Graphics;
 using SharpEngine.Core;
 using SharpEngine.Managers;
@@ -31,6 +30,8 @@ public class TileMapComponent: Component
     private List<Layer> _layers = new();
     private string _tilemap;
     private TransformComponent _transformComponent;
+    private Texture2D _internalTexture;
+    private bool _loaded;
 
     /// <summary>
     /// Initialise le Composant.
@@ -42,28 +43,26 @@ public class TileMapComponent: Component
         TileMap = tilemap;
     }
 
-    public void UpdateTileMap(string tilemap)
+    private void UpdateTileMap(string tilemap)
     {
         Map = GetWindow().TileMapManager.GetMap(tilemap);
         
         _layers = new List<Layer>();
         
-        if (_transformComponent == null || Map.Map.Layers.Count == 0) return;
+        if (Map.Map.Layers.Count == 0) return;
 
-        var compPosition = Map.Map.Infinite
-            ? _transformComponent.Position
-            : new Vec2(_transformComponent.Position.X - Map.TileSize.X * Map.Size.X * _transformComponent.Scale.X / 2,
-                _transformComponent.Position.Y - Map.TileSize.Y * Map.Size.Y * _transformComponent.Scale.Y / 2);
-
-        foreach (var layer in Map.Map.Layers.Where(layer => layer.Data != null))
+        foreach (var layer in Map.Map.Layers)
         {
+            if (layer.Data == null)
+                continue;
+            
             var lay = new Layer();
             for (var i = 0; i < layer.Data!.Tiles.Count; i++)
             {
                 var position =
                     new Vec2(
-                        compPosition.X + Map.TileSize.X * _transformComponent.Scale.X * Convert.ToInt32(i % Convert.ToInt32(Map.Size.X)),
-                        compPosition.Y + Map.TileSize.Y * _transformComponent.Scale.Y * Convert.ToInt32(i / Convert.ToInt32(Map.Size.X)));
+                        Map.TileSize.X * Convert.ToInt32(i % Convert.ToInt32(Map.Size.X)),
+                        Map.TileSize.Y * Convert.ToInt32(i / Convert.ToInt32(Map.Size.X)));
                 lay.Tiles.Add(new Tile(position, Map.GetTile(layer.Data.Tiles[i])));
             }
 
@@ -75,16 +74,17 @@ public class TileMapComponent: Component
                 for (var i = 0; i < chunk.Tiles.Count; i++)
                 {
                     var position = new Vec2(
-                        compPosition.X + x * Map.TileSize.X * _transformComponent.Scale.X +
-                        Map.TileSize.X * _transformComponent.Scale.X * Convert.ToInt32(i % Convert.ToInt32(chunk.Width)),
-                        compPosition.Y + y * Map.TileSize.Y * _transformComponent.Scale.Y +
-                        Map.TileSize.Y * _transformComponent.Scale.Y * Convert.ToInt32(i / Convert.ToInt32(chunk.Width)));
+                        x * Map.TileSize.X + Map.TileSize.X * Convert.ToInt32(i % Convert.ToInt32(chunk.Width)),
+                        y * Map.TileSize.Y + Map.TileSize.Y * Convert.ToInt32(i / Convert.ToInt32(chunk.Width)));
                     chun.Tiles.Add(new Tile(position, Map.GetTile(chunk.Tiles[i])));
                 }
                 lay.Chunks.Add(chun);
             }
             _layers.Add(lay);
         }
+        
+        if(_loaded)
+            UpdateTexture();
     }
 
     public override void Initialize()
@@ -93,16 +93,24 @@ public class TileMapComponent: Component
         UpdateTileMap(_tilemap);
     }
 
-    public override void Draw(GameTime gameTime)
+    public override void LoadContent()
     {
-        base.Draw(gameTime);
-        if (_transformComponent == null || _layers.Count == 0) return;
-        
-        var originPosition = new Vec2(
-            Map.TileSize.X * _transformComponent.Scale.X / 2,
-            Map.TileSize.Y * _transformComponent.Scale.Y / 2
-            );
+        base.LoadContent();
+        UpdateTexture();
+        _loaded = true;
+    }
 
+    public void UpdateTexture()
+    {
+        var target2D = new RenderTarget2D(Entity.Scene.Window.InternalGame.GraphicsDevice,
+            (int)(Map.TileSize.X * Map.Size.X), (int)(Map.TileSize.Y * Map.Size.Y), false,
+            GetWindow().InternalGame.GraphicsDevice.PresentationParameters.BackBufferFormat,
+            GetWindow().InternalGame.GraphicsDevice.PresentationParameters.DepthStencilFormat);
+        
+        Entity.Scene.Window.InternalGame.SpriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend, samplerState: SamplerState.PointClamp);
+        Entity.Scene.Window.InternalGame.GraphicsDevice.SetRenderTarget(target2D);
+        Entity.Scene.Window.InternalGame.GraphicsDevice.Clear(Color.Transparent);
+        
         var layerNb = 0;
         foreach (var layer in _layers)
         {
@@ -111,9 +119,9 @@ public class TileMapComponent: Component
                 if(tile.Type == null) continue;
                 
                 var texture = Entity.Scene.Window.TextureManager.GetTexture(tile.Type.Value.Source);
-                Renderer.RenderTexture(Entity.Scene.Window, texture, tile.Position - CameraManager.Position,
-                    tile.Type.Value.SourceRect, Color.White, 0, originPosition, _transformComponent.Scale,
-                    SpriteEffects.None, _transformComponent.LayerDepth + 0.00001f * layerNb);
+                Renderer.RenderTexture(Entity.Scene.Window, texture, tile.Position,
+                    tile.Type.Value.SourceRect, Color.White, 0, Vec2.Zero, Vec2.One, 
+                    SpriteEffects.None, layerNb * 0.0001f, true);
             }
 
             foreach (var chunk in layer.Chunks)
@@ -123,14 +131,32 @@ public class TileMapComponent: Component
                     if(tile.Type == null) continue;
                     
                     var texture = Entity.Scene.Window.TextureManager.GetTexture(tile.Type.Value.Source);
-                    Renderer.RenderTexture(Entity.Scene.Window, texture, tile.Position - CameraManager.Position,
-                        tile.Type.Value.SourceRect, Color.White, 0, originPosition, _transformComponent.Scale,
-                        SpriteEffects.None, _transformComponent.LayerDepth + 0.00001f * layerNb);
+                    Renderer.RenderTexture(Entity.Scene.Window, texture, tile.Position,
+                        tile.Type.Value.SourceRect, Color.White, 0, Vec2.Zero, Vec2.One,
+                        SpriteEffects.None, layerNb * 0.0001f, true);
                 }
             }
 
             layerNb++;
         }
+
+        Entity.Scene.Window.InternalGame.SpriteBatch.End();
+        Entity.Scene.Window.InternalGame.SpriteBatch.Begin();
+        Entity.Scene.Window.InternalGame.GraphicsDevice.SetRenderTarget(null);
+        Entity.Scene.Window.InternalGame.SpriteBatch.End();
+        _internalTexture = target2D;
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        base.Draw(gameTime);
+        if (_transformComponent == null || _layers.Count == 0) return;
+
+        var originPosition = new Vec2(Map.TileSize.X * Map.Size.X / 2, Map.TileSize.Y * Map.Size.Y / 2);
+        Renderer.RenderTexture(Entity.Scene.Window, _internalTexture,
+            _transformComponent.Position - CameraManager.Position, null, Color.White, 0, originPosition,
+            _transformComponent.Scale, SpriteEffects.None, _transformComponent.LayerDepth);
+
 
     }
 

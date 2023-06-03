23870b98235536be971055d1e026e6e45917e23f
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-03 21:33:44 +0200

    Update Window.cs

--- SharpEngine/Window.cs
+++ SharpEngine/Window.cs
@@ -9,30 +9,91 @@ namespace SharpEngine;
 
 public class Window
 {
-    public Color BackgroundColor;
-    private SeImGui _seImGui;
-
-    public Window(int width, int height, string title, Color? backgroundColor = null) : 
-        this(new Vec2I(width, height), title, backgroundColor) {}
-    
-    public Window(Vec2I screenSize, string title, Color? backgroundColor = null)
+    public string Title
     {
+        get => _title;
+        set
+        {
+            _title = value;
+            Raylib.SetWindowTitle(value);
+        }
+    }
+
+    public Vec2I ScreenSize
+    {
+        get => _screenSize;
+        set
+        {
+            _screenSize = value;
+            Raylib.SetWindowSize(value.X, value.Y);
+        }
+    }
+
+    public Vec2I Position
+    {
+        get => Raylib.GetWindowPosition();
+        set => Raylib.SetWindowPosition(value.X, value.Y);
+    }
+
+    public Color BackgroundColor;
+    public Func<bool> StartCallback;
+    public Func<bool> StopCallback;
+    public Action<Window> RenderImGui;
+    public bool Debug;
+
+    private Vec2I _screenSize;
+    private string _title;
+    private SeImGui _seImGui;
+    private bool _closeWindow = false;
+
+    public Window(int width, int height, string title, Color? backgroundColor = null, bool debug = false) : 
+        this(new Vec2I(width, height), title, backgroundColor, debug) {}
+    
+    public Window(Vec2I screenSize, string title, Color? backgroundColor = null, bool debug = false)
+    {
+        _title = title;
+        _screenSize = screenSize;
         BackgroundColor = backgroundColor ?? Color.Black;
+        Debug = debug;
+        
         Raylib.InitWindow(screenSize.X, screenSize.Y, title);
         
         _seImGui = new SeImGui();
         _seImGui.Load(screenSize.X, screenSize.Y);
     }
 
+    public void TakeScreenshot(string path) => Raylib.TakeScreenshot(path);
+
     public void Run()
     {
-        while (!Raylib.WindowShouldClose())
+        if(StartCallback != null && !StartCallback())
+            return;
+        
+        while (!Raylib.WindowShouldClose() && !_closeWindow)
         {
+            // UPDATE
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_A))
+            {
+                BackgroundColor = Color.Black;
+                Title = "Ceci est un test";
+                Position = new Vec2I(150, 200);
+                ScreenSize = new Vec2I(600, 800);
+            }
+
             _seImGui.Update(Raylib.GetFrameTime());
+            
+            
+            // DRAW IMGUI
+            if(Debug)
+                RenderImGui?.Invoke(this);
+            
+            // DRAW
             Raylib.BeginDrawing();
             Raylib.ClearBackground(BackgroundColor);
             
+            if(Debug)
                 _seImGui.Draw();
+            
             Raylib.EndDrawing();
         }
         
@@ -41,6 +102,7 @@ public class Window
 
     public void Stop()
     {
-        Raylib.CloseWindow();
+        if (StopCallback == null || StopCallback())
+            _closeWindow = true;
     }
 }

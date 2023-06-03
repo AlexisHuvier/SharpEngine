8c7acbcd019e4af9380c261864cf156cbd461820
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-03 21:34:51 +0200

    Create DebugManager and InputManager

---
+++ SharpEngine/Manager/DebugManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ImGuiNET;
+using Raylib_cs;
+
+namespace SharpEngine.Manager;
+
+public static class DebugManager
+{
+    public static int FrameRate => Raylib.GetFPS();
+    public static long GcMemory => GC.GetTotalMemory(false);
+    public const string SeVersion = "0.20.0";
+
+    public static void CreateSeImGuiWindow()
+    {
+        ImGui.Begin("SharpEngine Debug");
+        ImGui.Text($"SharpEngine Version : {SeVersion}");
+        ImGui.Separator();
+        ImGui.Text($"FPS from ImGui : {1000.0/ImGui.GetIO().Framerate:.000}ms/frame ({ImGui.GetIO().Framerate} FPS)");
+        ImGui.Text($"FPS from SE : {1000.0/FrameRate:.000}ms/frame ({FrameRate} FPS)");
+        ImGui.Text($"GC Memory : {GcMemory/1000000.0:.000} mo");
+        ImGui.End();
+    }
+}
---
+++ SharpEngine/Manager/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Raylib_cs;
+using SharpEngine.Math;
+using SharpEngine.Utils.Input;
+using MouseButton = SharpEngine.Utils.Input.MouseButton;
+
+namespace SharpEngine.Manager;
+
+public static class InputManager
+{
+    public static Key[] GetPressedKeys()
+    {
+        var keys = new List<Key>();
+        var key = Raylib.GetKeyPressed();
+        while (key > 0)
+        {
+            System.Console.WriteLine(key);
+            keys.Add((Key)key);
+            key = Raylib.GetKeyPressed();
+        }
+
+        return keys.ToArray();
+    }
+    
+    public static bool IsKeyDown(Key key) => Raylib.IsKeyDown(key.ToRayLib());
+    public static bool IsKeyUp(Key key) => Raylib.IsKeyUp(key.ToRayLib());
+    public static bool IsKeyPressed(Key key) => Raylib.IsKeyPressed(key.ToRayLib());
+    public static bool IsKeyReleased(Key key) => Raylib.IsKeyReleased(key.ToRayLib());
+
+    public static bool IsMouseButtonDown(MouseButton button) => Raylib.IsMouseButtonDown(button.ToRayLib());
+    public static bool IsMouseButtonUp(MouseButton button) => Raylib.IsMouseButtonUp(button.ToRayLib());
+    public static bool IsMouseButtonPressed(MouseButton button) => Raylib.IsMouseButtonPressed(button.ToRayLib());
+    public static bool IsMouseButtonReleased(MouseButton button) => Raylib.IsMouseButtonReleased(button.ToRayLib());
+
+    public static Vec2 GetMousePosition() => Raylib.GetMousePosition();
+    public static void SetMousePosition(Vec2I position) => Raylib.SetMousePosition(position.X, position.Y);
+    public static float GetMouseWheelMove() => Raylib.GetMouseWheelMove();
+
+    public static bool IsGamePadConnected(int index) => Raylib.IsGamepadAvailable(index);
+    public static bool IsGamePadButtonDown(int index, GamePadButton button) =>
+        Raylib.IsGamepadButtonDown(index, button.ToRayLib());
+    public static bool IsGamePadButtonUp(int index, GamePadButton button) =>
+        Raylib.IsGamepadButtonUp(index, button.ToRayLib());
+    public static bool IsGamePadButtonPressed(int index, GamePadButton button) =>
+        Raylib.IsGamepadButtonPressed(index, button.ToRayLib());
+    public static bool IsGamePadButtonReleased(int index, GamePadButton button) =>
+        Raylib.IsGamepadButtonReleased(index, button.ToRayLib());
+
+    public static float GetGamePadAxis(int index, GamePadAxis axis) =>
+        Raylib.GetGamepadAxisMovement(index, axis.ToRayLib());
+}

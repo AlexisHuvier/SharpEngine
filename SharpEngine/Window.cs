e40544d354226dac2940fcb379ced9071516ad9c
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-03 11:34:31 +0200

    Create basic Window

---
+++ SharpEngine/Window.cs
@@ -0,0 +1,37 @@
+﻿using Raylib_cs;
+using SharpEngine.Math;
+using Color = SharpEngine.Utils.Color;
+
+namespace SharpEngine;
+
+public class Window
+{
+    public Color BackgroundColor;
+
+    public Window(int width, int height, string title, Color? backgroundColor = null) : 
+        this(new Vec2I(width, height), title, backgroundColor) {}
+    
+    public Window(Vec2I screenSize, string title, Color? backgroundColor = null)
+    {
+        BackgroundColor = backgroundColor ?? Color.Black;
+        Raylib.InitWindow(screenSize.X, screenSize.Y, title);
+    }
+
+    public void Run()
+    {
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(BackgroundColor);
+            
+            Raylib.EndDrawing();
+        }
+        
+        Raylib.CloseWindow();
+    }
+
+    public void Stop()
+    {
+        Raylib.CloseWindow();
+    }
+}

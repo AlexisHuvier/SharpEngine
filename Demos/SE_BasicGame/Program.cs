ef6abc8e035ba17cabdfd5bd17c8f928ac9384eb
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-03 11:34:38 +0200

    Update Demo

--- Demos/SE_BasicGame/Program.cs
+++ Demos/SE_BasicGame/Program.cs
@@ -1,8 +1,13 @@
-﻿namespace SE_BasicWindow;
+﻿using SharpEngine;
+using SharpEngine.Utils;
+
+namespace SE_BasicWindow;
 
 internal static class Program
 {
     private static void Main()
     {
+        var window = new Window(800, 600, "SE Raylib", Color.CornflowerBlue);
+        window.Run();
     }
 }

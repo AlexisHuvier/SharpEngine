3a27e1c1ccd3e23df903094ab48eb2af0580ee72
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-03 21:35:16 +0200

    Update Demos

--- Demos/SE_BasicGame/Program.cs
+++ Demos/SE_BasicGame/Program.cs
@@ -1,4 +1,6 @@
-﻿using SharpEngine;
+﻿using ImGuiNET;
+using SharpEngine;
+using SharpEngine.Manager;
 using SharpEngine.Utils;
 
 namespace SE_BasicWindow;
@@ -7,7 +9,19 @@ internal static class Program
 {
     private static void Main()
     {
-        var window = new Window(800, 600, "SE Raylib", Color.CornflowerBlue);
+        var window = new Window(800, 600, "SE Raylib", Color.CornflowerBlue, true)
+        {
+            RenderImGui = win =>
+            {
+                DebugManager.CreateSeImGuiWindow();
+                ImGui.Begin("Basic Game Debug");
+                if(ImGui.Button("Stop Window"))
+                    win.Stop();
+                if(ImGui.Button("Screenshot"))
+                    win.TakeScreenshot("test.png");
+                ImGui.End();
+            }
+        };
         window.Run();
     }
 }

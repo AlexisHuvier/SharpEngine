c2298fef78105bc1bad9c43099218b36f58ede9c
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-09 21:52:24 +0200

    Add Log Extensions

--- SharpEngine/Utils/Extensions.cs
+++ SharpEngine/Utils/Extensions.cs
@@ -10,9 +10,11 @@ internal static class Extensions
     public static KeyboardKey ToRayLib(this Key key) => (KeyboardKey)key;
     public static GamepadButton ToRayLib(this GamePadButton button) => (GamepadButton)button;
     public static GamepadAxis ToRayLib(this GamePadAxis axis) => (GamepadAxis)axis;
+    public static TraceLogLevel ToRayLib(this LogLevel logLevel) => (TraceLogLevel)logLevel;
     
     public static MouseButton ToSe(this Raylib_cs.MouseButton button) => (MouseButton)button;
     public static Key ToSe(this KeyboardKey key) => (Key)key;
     public static GamePadButton ToSe(this GamepadButton button) => (GamePadButton)button;
     public static GamePadAxis ToSe(this GamepadAxis axis) => (GamePadAxis)axis;
+    public static LogLevel ToSe(this TraceLogLevel logLevel) => (LogLevel)logLevel;
 }

c688ec33e99b3e3f1fe9da4f50ce72297b6b91d6
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-18 13:59:01 +0200

    Update Demo

--- Demos/SE_BasicGame/MyScene.cs
+++ Demos/SE_BasicGame/MyScene.cs
@@ -11,9 +11,15 @@ internal class MyScene: Scene
     public MyScene()
     {
         var e = AddEntity(new Entity());
-        e.AddComponent(new TransformComponent(new Vec2(400)));
+        e.AddComponent(new TransformComponent(new Vec2(400), new Vec2(2)));
         e.AddComponent(new SpriteComponent("KnightM"));
-        e.AddComponent(new RectComponent(Color.Black, new Vec2(300), true, new Vec2(-100, -200)));
+        e.AddComponent(new SpriteSheetComponent("KnightM", new Vec2(16, 28), new List<Animation>
+        {
+            new("hit", new List<uint> { 0 }, 0.1f),
+            new("idle", new List<uint> { 1, 2, 3, 4 }, 0.1f),
+            new("run", new List<uint> { 5, 6, 7, 8 }, 0.1f)
+        }, "idle", true, new Vec2(200, -100)));
+        e.AddComponent(new RectComponent(Color.Black, new Vec2(25), true, new Vec2(-100, -200)));
         e.AddComponent(new TextComponent("test", color: Color.Red, fontSize: 50, offset: new Vec2(-100)));
         e.AddComponent(new TextComponent("test", "basic", color: Color.Blue, fontSize: 50, offset: new Vec2(100)));
     }

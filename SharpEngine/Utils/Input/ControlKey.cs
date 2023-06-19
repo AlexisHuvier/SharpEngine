b9a558ec1893e8bb166310e84927025e90e58934
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-19 21:13:17 +0200

    Create Control System

---
+++ SharpEngine/Utils/Input/ControlKey.cs
@@ -0,0 +1,27 @@
+﻿namespace SharpEngine.Utils.Input;
+
+/// <summary>
+/// Control Keys
+/// </summary>
+public enum ControlKey
+{
+    /// <summary>
+    /// Up Key
+    /// </summary>
+    Up,
+    
+    /// <summary>
+    /// Down Key
+    /// </summary>
+    Down,
+    
+    /// <summary>
+    /// Left Key
+    /// </summary>
+    Left,
+    
+    /// <summary>
+    /// Right Key
+    /// </summary>
+    Right
+}
---
+++ SharpEngine/Utils/Input/ControlType.cs
@@ -0,0 +1,32 @@
+﻿namespace SharpEngine.Utils.Input;
+
+/// <summary>
+/// Type of Controls
+/// </summary>
+public enum ControlType
+{
+    /// <summary>
+    /// Follow Mouse
+    /// </summary>
+    MouseFollow,
+    
+    /// <summary>
+    /// Only Left and Right Movements
+    /// </summary>
+    LeftRight,
+    
+    /// <summary>
+    /// Only Up and Down Movements
+    /// </summary>
+    UpDown,
+    
+    /// <summary>
+    /// Up, Down, Left and Right Movements
+    /// </summary>
+    FourDirection,
+    
+    /// <summary>
+    /// Classic Platformer Movements
+    /// </summary>
+    ClassicJump
+}

35d8ecc4e118cdc7865f8760c0d940283dab94ff
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-22 23:02:23 +0200

    Update Demo

--- Demos/SE_BasicGame/MyScene.cs
+++ Demos/SE_BasicGame/MyScene.cs
@@ -1,8 +1,7 @@
 ﻿using SharpEngine;
-using SharpEngine.Component;
-using SharpEngine.Entity;
 using SharpEngine.Math;
 using SharpEngine.Utils;
+using SharpEngine.Widget;
 
 namespace SE_BasicWindow;
 
@@ -10,17 +9,9 @@ internal class MyScene: Scene
 {
     public MyScene()
     {
-        var e = AddEntity(new Entity());
-        e.AddComponent(new TransformComponent(new Vec2(400), new Vec2(2)));
-        e.AddComponent(new SpriteComponent("KnightM"));
-        e.AddComponent(new SpriteSheetComponent("KnightM", new Vec2(16, 28), new List<Animation>
-        {
-            new("hit", new List<uint> { 0 }, 0.1f),
-            new("idle", new List<uint> { 1, 2, 3, 4 }, 0.1f),
-            new("run", new List<uint> { 5, 6, 7, 8 }, 0.1f)
-        }, "idle", true, new Vec2(200, -100)));
-        e.AddComponent(new RectComponent(Color.Black, new Vec2(25), true, new Vec2(-100, -200)));
-        e.AddComponent(new TextComponent("test", color: Color.Red, fontSize: 50, offset: new Vec2(-100)));
-        e.AddComponent(new TextComponent("test", "basic", color: Color.Blue, fontSize: 50, offset: new Vec2(100)));
+        AddWidget(new ColorRect(new Vec2(50), new Vec2(25), Color.Aqua, 15));
+        AddWidget(new Label(new Vec2(100), "Test Label", "basic", Color.Red, 10));
+        AddWidget(new Label(new Vec2(200), "Test Label Multiline\nTest !", "basic", Color.Red));
+        AddWidget(new Label(new Vec2(300), "Test Label Multiline\nTest !", "basic", Color.Red, 0, true));
     }
 }

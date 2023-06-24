c16f88eabe2352fcfdb18f7d2fe22db4578a0682
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-24 14:00:13 +0200

    feat(demo): update

--- Demos/SE_BasicGame/MyScene.cs
+++ Demos/SE_BasicGame/MyScene.cs
@@ -1,4 +1,5 @@
 ﻿using SharpEngine;
+using SharpEngine.Manager;
 using SharpEngine.Math;
 using SharpEngine.Utils;
 using SharpEngine.Widget;
@@ -7,11 +8,20 @@ namespace SE_BasicWindow;
 
 internal class MyScene: Scene
 {
+    private Checkbox _checkbox;
+    
     public MyScene()
     {
         AddWidget(new ColorRect(new Vec2(50), new Vec2(25), Color.Aqua, 15));
-        AddWidget(new Label(new Vec2(100), "Test Label", "basic", Color.Red, 10));
-        AddWidget(new Label(new Vec2(200), "Test Label Multiline\nTest !", "basic", Color.Red));
-        AddWidget(new Label(new Vec2(300), "Test Label Multiline\nTest !", "basic", Color.Red, 0, true));
+        AddWidget(new Frame(new Vec2(200), new Vec2(50), new Vec2(5), Color.Red));
+        _checkbox = AddWidget(new Checkbox(new Vec2(400)));
+
+        AddWidget(new TextureButton(new Vec2(600, 400), "Heyo", "basic", "KnightM", new Vec2(200, 60), Color.Red)).Clicked += OnClicked;
+    }
+
+    private void OnClicked(object? sender, EventArgs e)
+    {
+        if(sender != null)
+            DebugManager.Log(LogLevel.LogDebug, $"BUTTON CLICK : {_checkbox.IsChecked}");
     }
 }

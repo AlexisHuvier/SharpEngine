3ed46c01de6affd892652b410a041cd20b0cfba9
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-25 12:39:52 +0200

    feat(event): Add ValueEventArgs

---
+++ SharpEngine/Utils/EventArgs/ValueEventArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpEngine.Utils.EventArgs;
+
+/// <summary>
+/// Event Args which have old and new value 
+/// </summary>
+/// <typeparam name="T">Type of Value</typeparam>
+public class ValueEventArgs<T>: System.EventArgs
+{
+    /// <summary>
+    /// Old Value
+    /// </summary>
+    public T OldValue { get; init; }
+    
+    /// <summary>
+    /// New Value
+    /// </summary>
+    public T NewValue { get; init; }
+}

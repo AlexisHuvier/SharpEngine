4818c1e568ab0b125371ce1349cb38626ae94e2d
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-28 13:54:21 +0200

    update(demo)

--- Demos/SE_BasicGame/MyScene.cs
+++ Demos/SE_BasicGame/MyScene.cs
@@ -1,7 +1,6 @@
 ﻿using SharpEngine;
-using SharpEngine.Manager;
 using SharpEngine.Math;
-using SharpEngine.Utils;
+using SharpEngine.Utils.Widget;
 using SharpEngine.Widget;
 
 namespace SE_BasicWindow;
@@ -10,14 +9,9 @@ internal class MyScene : Scene
 {
     public MyScene()
     {
-        AddWidget(new ColorRect(new Vec2(50), new Vec2(25), Color.Aqua, 15));
-        AddWidget(new Frame(new Vec2(200), new Vec2(50), new Vec2(5), Color.Red));
-        var checkbox = AddWidget(new Checkbox(new Vec2(400)));
-        AddWidget(new LineInput(new Vec2(200, 400), "BASE", "basic")).ValueChanged += (_, args) =>
-            DebugManager.Log(LogLevel.LogDebug, $"Old : {args.OldValue}, New : {args.NewValue}");
-
-        AddWidget(new TextureButton(new Vec2(600, 400), "Heyo", "basic", "KnightM", new Vec2(200, 60), Color.Red))
-            .Clicked += (_, _) => DebugManager.Log(LogLevel.LogDebug, checkbox.IsChecked.ToString());
-
+        AddWidget(new Label(new Vec2(100), "Test Multi\nLine", "basic", centerAllLines: false));
+        AddWidget(new Label(new Vec2(300, 100), "Test Multi\nLine", "basic", centerAllLines: true));
+        AddWidget(new Label(new Vec2(100, 500), "Test Multi\nLine", "basic", style: LabelStyle.Strike | LabelStyle.Underline, centerAllLines: false));
+        AddWidget(new Label(new Vec2(300, 500), "Test Multi\nLine", "basic", style: LabelStyle.Strike | LabelStyle.Underline, centerAllLines: true));
     }
 }

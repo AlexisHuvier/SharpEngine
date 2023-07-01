67e3666c800a6af32101ebc684417e63ed3f5a2b
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-07-01 17:32:21 +0200

    update(demo)

--- Demos/SE_BasicGame/MyScene.cs
+++ Demos/SE_BasicGame/MyScene.cs
@@ -1,6 +1,5 @@
 ﻿using SharpEngine;
 using SharpEngine.Math;
-using SharpEngine.Utils.Widget;
 using SharpEngine.Widget;
 
 namespace SE_BasicWindow;
@@ -9,9 +8,6 @@ internal class MyScene : Scene
 {
     public MyScene()
     {
-        AddWidget(new Label(new Vec2(100), "Test Multi\nLine", "basic", centerAllLines: false));
-        AddWidget(new Label(new Vec2(300, 100), "Test Multi\nLine", "basic", centerAllLines: true));
-        AddWidget(new Label(new Vec2(100, 500), "Test Multi\nLine", "basic", style: LabelStyle.Strike | LabelStyle.Underline, centerAllLines: false));
-        AddWidget(new Label(new Vec2(300, 500), "Test Multi\nLine", "basic", style: LabelStyle.Strike | LabelStyle.Underline, centerAllLines: true));
+        AddWidget(new MultiLineInput(new Vec2(300), "", "basic"));
     }
 }

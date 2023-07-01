1d328e8fc859f3ae5cc62352d8f40c22e7552c7e
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-07-01 17:31:33 +0200

    feat(frame): Use DrawRectangleLinesEx

--- SharpEngine/Widget/Frame.cs
+++ SharpEngine/Widget/Frame.cs
@@ -22,7 +22,7 @@ public class Frame: Widget
     /// <summary>
     /// Size of Frame Border 
     /// </summary>
-    public Vec2 BorderSize;
+    public int BorderSize;
     
     /// <summary>
     /// Color of Frame Background
@@ -34,15 +34,15 @@ public class Frame: Widget
     /// </summary>
     /// <param name="position">Frame Position</param>
     /// <param name="size">Frame Size</param>
-    /// <param name="borderSize">Frame Border Size (Vec2(3))</param>
+    /// <param name="borderSize">Frame Border Size (3)</param>
     /// <param name="borderColor">Frame Border Color (Color.Black)</param>
     /// <param name="backgroundColor">Frame Background Color (null)</param>
-    public Frame(Vec2 position, Vec2 size, Vec2? borderSize = null, Color? borderColor = null,
+    public Frame(Vec2 position, Vec2 size, int borderSize = 3, Color? borderColor = null,
         Color? backgroundColor = null) : base(position)
     {
         BorderColor = borderColor ?? Color.Black;
         Size = size;
-        BorderSize = borderSize ?? new Vec2(3);
+        BorderSize = borderSize;
         BackgroundColor = backgroundColor;
     }
 
@@ -54,24 +54,8 @@ public class Frame: Widget
         if(!Displayed || Size == Vec2.Zero) return;
 
         var position = RealPosition;
-        if (BackgroundColor == null)
-        {
-            Raylib.DrawRectangle((int)(position.X - Size.X / 2), (int)(position.Y - Size.Y / 2), (int)Size.X,
-                (int)BorderSize.Y, BorderColor);
-            Raylib.DrawRectangle((int)(position.X - Size.X / 2), (int)(position.Y - Size.Y / 2), (int)BorderSize.X,
-                (int)Size.Y, BorderColor);
-            Raylib.DrawRectangle((int)(position.X + (Size.X - BorderSize.X) - Size.X / 2),
-                (int)(position.Y - Size.Y / 2), (int)BorderSize.X,(int)Size.Y, BorderColor);
-            Raylib.DrawRectangle((int)(position.X - Size.X / 2),
-                (int)(position.Y + (Size.Y - BorderSize.Y) - Size.Y / 2), (int)Size.X, (int)BorderSize.Y, BorderColor);
-        }
-        else
-        {
-            Raylib.DrawRectanglePro(new Rectangle(position.X, position.Y, Size.X, Size.Y), Size / 2, 0, BorderColor);
-            var innerSize = new Vec2(Size.X - BorderSize.X * 2, Size.Y - BorderSize.Y * 2);
-            Raylib.DrawRectanglePro(
-                new Rectangle(position.X, position.Y, innerSize.X, innerSize.Y),
-                innerSize / 2, 0, BackgroundColor.Value);
-        }
+        if (BackgroundColor != null)
+            Raylib.DrawRectanglePro(new Rectangle(position.X, position.Y, Size.X, Size.Y), Size / 2, 0, BackgroundColor.Value);
+        Raylib.DrawRectangleLinesEx(new Rectangle(position.X - Size.X / 2, position.Y - Size.Y / 2, Size.X,Size.Y), BorderSize, BorderColor);
     }
 }

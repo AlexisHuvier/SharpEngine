8ccefc982d90a34ea81f43fb9b92b75309fa1917
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-07-28 20:36:03 +0200

    v1.4.8

--- SharpEngine/Manager/DebugManager.cs
+++ SharpEngine/Manager/DebugManager.cs
@@ -26,7 +26,7 @@ public static class DebugManager
     /// <summary>
     /// SharpEngine Version
     /// </summary>
-    public const string SeVersion = "1.4.7";
+    public const string SeVersion = "1.4.8";
 
     /// <summary>
     /// Create ImGui Window for SharpEngine

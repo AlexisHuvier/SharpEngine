fcfa26a93d8e4adde69e972f7b64f6c574c82af6
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-08-11 16:20:15 +0200

    update(demo)

--- Demos/SE_BasicGame/Program.cs
+++ Demos/SE_BasicGame/Program.cs
@@ -8,7 +8,7 @@ internal static class Program
 {
     private static void Main()
     {
-        var window = new Window(1280, 920, "SE Raylib", Color.CornflowerBlue, null, true)
+        var window = new Window(1280, 920, "SE Raylib", Color.CornflowerBlue, null, true, true, true)
         {
             RenderImGui = DebugManager.CreateSeImGuiWindow
         };
